5075bbb13a4075573aeb5a01864e435aa31ad0c4
Aswin Lal M <aswnss.m@gmail.com>
2026-03-12 12:33:42 +0530

    Remove font loading from days-in-year route and simplify ImageResponse configuration

--- app/types/days-in-year/file/route.tsx
+++ app/types/days-in-year/file/route.tsx
@@ -8,20 +8,11 @@ import { createModelScaler } from "@/lib/utils";
 
 export async function GET(request: NextRequest) {
 	const { searchParams } = request.nextUrl;
-  const font = await fetch(new URL("/Inter-variable.ttf", import.meta.url)).then(res => res.arrayBuffer());
   const response = new ImageResponse(
     <div>Hello!</div>,
  {
   width: 1200,
   height: 630,
-  fonts: [
-    {
-      "name": "Inter",
-      "data": font,
-      "style": "normal",
-      "weight": 400,
-    }
-  ]
  }
   )
 	  return response
